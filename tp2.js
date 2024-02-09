6bcfd1af99ea155d51341a1a13b1c36e778abc5a
Your NameVirginiePEREZ <p.virgini@gmail.com>
2024-02-09 11:46:49 +0100

    TP1 et 2

---
+++ tp2.js
@@ -0,0 +1,14 @@
+// Demande à l'utilisateur d'entrer la distance parcourue en mètres
+let distance = parseFloat(prompt("Entrez la distance parcourue en mètres :"));
+
+// Demande à l'utilisateur d'entrer le temps en secondes
+let temps = parseFloat(prompt("Entrez le temps en secondes :"));
+
+// Calcul de la vitesse en mètres par seconde
+let vitesse = distance / temps;
+
+// Affichage du résultat avec alert
+alert("La vitesse est de : " + vitesse + " m/s");
+
+// Affichage du résultat dans la console
+console.log("La vitesse est de : " + vitesse + " m/s");
