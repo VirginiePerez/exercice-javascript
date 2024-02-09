9dcb17633f4fde5b833ee20ea2983ccd81832bdb
Your NameVirginiePEREZ <p.virgini@gmail.com>
2024-02-09 12:28:32 +0100

    gestion exception tp2

--- tp1.js
+++ tp1.js
@@ -1,16 +1,15 @@
-// Déclaration des variables avec des valeurs de type number
+
 let variable1 = 10;
 let variable2 = 20;
 
-// Déclaration d'une constante égale à la somme des deux variables
+
 const somme = variable1 + variable2;
 
-// Affichage d'un message d'alerte
+
 alert("Bienvenue sur la page de formulaire");
 
-// Affichage du contenu des variables dans la console du navigateur
+
 console.log("Contenu de la variable 1 : " + variable1);
 console.log("Contenu de la variable 2 : " + variable2);
 
-// Affichage du résultat du calcul dans la console du navigateur
 console.log("Le résultat est : " + somme);
--- tp2.js
+++ tp2.js
@@ -3,12 +3,19 @@ let distance = parseFloat(prompt("Entrez la distance parcourue en mètres :"));
 
 // Demande à l'utilisateur d'entrer le temps en secondes
 let temps = parseFloat(prompt("Entrez le temps en secondes :"));
+if(isNaN(distance )|| isNaN(temps))
+{
+  alert("Valeur invalide");
 
-// Calcul de la vitesse en mètres par seconde
-let vitesse = distance / temps;
+  // Calcul de la vitesse en mètres par seconde
+}else{let vitesse = distance / temps;
 
 // Affichage du résultat avec alert
 alert("La vitesse est de : " + vitesse + " m/s");
 
 // Affichage du résultat dans la console
 console.log("La vitesse est de : " + vitesse + " m/s");
+
+}
+ 
+
